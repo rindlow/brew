cbf0ba44526e5a7831ab7dcf17ec837194e13103
Henrik Rindlöw <henrik@rindlow.se>
2021-09-29 15:25:04 +0200

    Add max_malt_weight parameter

--- brew/profiles.py
+++ brew/profiles.py
@@ -3,9 +3,12 @@ class Profile:
     max_boil_volume = 0.0
     min_mash_volume = 0.0
     max_mash_volume = 0.0
+    max_malt_weight = 0.0
     mash_efficiency = 0.
     water_in_malt_ratio = 0.0
     kettle_deadspace = 0.0
+    power = 0.0
+    boil_off_rate = 0.0
 
 
 class Braumeister20l(Profile):
@@ -13,6 +16,7 @@ class Braumeister20l(Profile):
     max_boil_volume = 30.0
     min_mash_volume = 16.0
     max_mash_volume = 25.0
+    max_malt_weight = 6.0
     mash_efficiency = 0.82
     water_in_malt_ratio = 1.0
     kettle_deadspace = 4.0
@@ -25,6 +29,7 @@ class Braumeister50l(Profile):
     max_boil_volume = 60.0
     min_mash_volume = 40.0
     max_mash_volume = 55.0
+    max_malt_weight = 13.0
     mash_efficiency = 0.82
     water_in_malt_ratio = 1.0
     kettle_deadspace = 4.0
